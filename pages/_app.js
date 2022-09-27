5de71ccbdeb9bb0083fa81b7024410e76206bbf5
Apoorva Shukla <aposhukl@WKMZTB5622B1.global.publicisgroupe.net>
2022-09-27 18:39:55 +0530

    Change the meta description content

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Apoorva</title>
-        <meta name="description" content="A personal blog" />
+        <meta name="description" content="A portfolio website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
